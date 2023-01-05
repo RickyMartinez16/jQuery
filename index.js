d7b94052943115ea117031e860c163d5e956b729
Ricky Martinez <RMartinezW16@gmail.com>
2023-01-04 20:19:41 -0500

    button jquery

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 $("h1").css("color", "red")
+
+$("button").click(() => {
+    console.log("button clicked")
+})
+
+$("h1").click(() => {
+    $("h1").css("color", "blue")
+})
+
+$("input").keypress((event) => {
+    $("h1").text(event.key)
+})
